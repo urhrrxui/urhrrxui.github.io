925d2e20fb6311243423c67469e4feb6355ac3d7
urhrrxui <79515719+urhrrxui@users.noreply.github.com>
2021-02-24 21:57:55 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,38 @@
-const CACHE_NAME = 'Phone-Chat';
-let resourcesToCache = ["./", "./img/game.png"];
-self.addEventListener("install", e=>{ e.waitUntil( caches.open(CACHE_NAME).then(cache =>{ return cache.addAll(resourcesToCache); }) ); });
+// jshint esversion:6
+
+// Installing service workker
+const CACHE_NAME  = 'Simon-game';
+let resourcesToCache = ["./", "./img/game.png", "./game.js", "./sounds/wrong.mp3", "./sounds/blue.mp3", "./sounds/green.mp3", "./sounds/red.mp3", "./sounds/yellow.mp3", "./styles.css"];
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            return cache.addAll(resourcesToCache);
+        })
+    );
+});
+
 // Cache and return requests
-self.addEventListener("fetch", e=>{ e.respondWith( caches.match(e.request).then(response=>{ return response || fetch(e.request); }) ); });
-// Update a service worker 
-const cacheWhitelist = ['Phone-Chat'];
-self.addEventListener('activate', event => { event.waitUntil( caches.keys().then(cacheNames => { return Promise.all( cacheNames.map(cacheName => { if (cacheWhitelist.indexOf(cacheName) === -1) { return caches.delete(cacheName); } }) ); }) ); });
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
+
+// Update a service worker
+const cacheWhitelist = ['Simon-game'];
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
